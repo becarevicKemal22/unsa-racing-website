afc0b9210f8a2a9d858c225a0ffbb25e5046d16b
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2025-04-06 03:38:55 +0200

    dodan poppins font na stranicu; implementiran header

--- src/main.js
+++ src/main.js
@@ -1,2 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./style.css";
+
+/*
+    Ovaj event listener na svako ucitavanje stranice preuzme header.html i njegov html umece u header tag
+    Zbog toga se main.js mora dodati kao skripta na svaku stranicu websajta, a zajedno sa njom i header tag u koji ce
+    se ucitati html headera.
+ */
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('src/partials/header.html')
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector('header').innerHTML = html;
+
+            // Nakon ucitavanja headera se odavdje dodaje 'active' klasa na link na kojem se korisnik trenutno nalazi
+            const navLinks = document.querySelectorAll('.nav-link');
+            navLinks.forEach(link => {
+                const linkHref = link.getAttribute('href');
+                const currentPath = window.location.pathname;
+
+                if (currentPath === linkHref)
+                    link.classList.add('active');
+                 else
+                    link.classList.remove('active');
+
+            });
+        })
+        .catch(error => {
+            console.error('Greska pri ucitavanju headera:', error);
+        });
+});
+
