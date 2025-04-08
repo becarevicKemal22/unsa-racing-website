51081e6ad1ef57b93853e4ed2209adb20dbcd228
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2025-04-08 20:04:44 +0200

    implementirani dijelovi about us i what is formula student; header je sada proziran pri ucitavanju stranice

--- src/main.js
+++ src/main.js
@@ -5,23 +5,29 @@ import "./style.css";
 /*
     Ovaj event listener na svako ucitavanje stranice preuzme header.html i njegov html umece u header tag
     Zbog toga se main.js mora dodati kao skripta na svaku stranicu websajta, a zajedno sa njom i header tag u koji ce
-    se ucitati html headera.
+    se ucitati html headera. U ovoj funkciji se takodjer dodaju odgovarajuce CSS klase vezane za header (za aktivni
+    link i za prvobitno proziran header na Home stranici)
  */
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     fetch('src/partials/header.html')
         .then(response => response.text())
         .then(html => {
             document.querySelector('header').innerHTML = html;
 
-            // Nakon ucitavanja headera se odavdje dodaje 'active' klasa na link na kojem se korisnik trenutno nalazi
+            const currentPath = window.location.pathname;
+            if (currentPath === "/") {
+                document.querySelector('nav').classList.add("transparent-nav");
+            } else {
+                document.querySelector('nav').classList.add("scrolled-nav");
+            }
+
             const navLinks = document.querySelectorAll('.nav-link');
             navLinks.forEach(link => {
                 const linkHref = link.getAttribute('href');
-                const currentPath = window.location.pathname;
 
                 if (currentPath === linkHref)
                     link.classList.add('active');
-                 else
+                else
                     link.classList.remove('active');
 
             });
@@ -31,3 +37,17 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 });
 
+/*
+    Ovaj event listener sluzi kako bi se na Home stranici dodala pozadina headeru nakon sto korisnik scrolla. Klasa
+    transparent-nav se dodaje samo kada je korisnik na home ("/") URL-u, pa se zbog toga u event listeneru jedino
+    razmatra ta lokacija.
+ */
+window.addEventListener('scroll', () => {
+    if (window.location.pathname !== "/") return;
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 10) {
+        nav.classList.add('scrolled-nav');
+    } else {
+        nav.classList.remove('scrolled-nav');
+    }
+});
